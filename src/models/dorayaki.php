c49715f74a9f98f4acd669ddb29efb88e6b6672d
Frederic Ronaldi <frederic.ronaldi@gmail.com>
2021-10-09 12:39:32 +0700

    add crud for dorayaki

--- src/models/dorayaki.php
+++ src/models/dorayaki.php
@@ -12,6 +12,87 @@ class DorayakiModel
         $this->db = new Database();
     }
 
+    public function getAllDoriyakis()
+    {
+        $this->db->query('SELECT * FROM ' . DorayakiModel::$table);
+        return $this->db->resultSet();
+    }
+
+    public function getDoriyakiById($id)
+    {
+        $this->db->query('SELECT * FROM ' . DorayakiModel::$table . ' WHERE dorayaki_id = :dorayaki_id');
+        $this->db->bind(':dorayaki_id', $id);
+        return $this->db->single();
+    }
+
+    public function createDoriyaki($data)
+    {
+        $query = "INSERT INTO " . DorayakiModel::$table . " VALUES
+                  (NULL, :name, :description, :price, :stock, :thumbnail)";
+
+        $this->db->query($query);
+
+        $this->db->bind(':name', $data['name']);
+        $this->db->bind(':description', $data['description']);
+        $this->db->bind(':price', $data['price']);
+        $this->db->bind(':stock', $data['stock']);
+        $this->db->bind(':thumbnail', $data['thumbnail']);
+
+        $this->db->execute();
+
+        return $this->db->rowCount();
+    }
+
+    public function deleteDoriyaki($id)
+    {
+        $query = "DELETE FROM " . DorayakiModel::$table . " WHERE dorayaki_id = :dorayaki_id";
+
+        $this->db->query($query);
+        $this->db->bind(':user_id', $id);
+
+        $this->db->execute();
+
+        return $this->db->rowCount();
+    }
+
+    public function updateDoriyaki($data)
+    {
+        $query = "UPDATE " . DorayakiModel::$table . " SET
+                    name = :name,
+                    description = :description,
+                    price = :price,
+                    thumbnail = :thumbnail
+                  WHERE doriyaki_id = :doriyaki_id";
+
+        $this->db->query($query);
+
+        $this->db->bind(':doriyaki_id', $data['doriyaki_id']);
+        $this->db->bind(':name', $data['name']);
+        $this->db->bind(':description', $data['description']);
+        $this->db->bind(':price', $data['price']);
+        $this->db->bind(':thumbnail', $data['thumbnail']);
+
+        $this->db->execute();
+
+        return $this->db->rowCount();
+    }
+
+    public function updateStock($data)
+    {
+        $query = "UPDATE " . DorayakiModel::$table . " SET
+                    stock = :stock
+                  WHERE doriyaki_id = :doriyaki_id";
+
+        $this->db->query($query);
+
+        $this->db->bind(':doriyaki_id', $data['doriyaki_id']);
+        $this->db->bind(':stock', $data['stock']);
+
+        $this->db->execute();
+
+        return $this->db->rowCount();
+    }
+
     public static function createDorayakiDatabase(SQLite3 $db): void
     {
         $db->exec("
