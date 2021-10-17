33ad6e2b28c1586e6bb2d7083ea57c8bb9bcbe76
Frederic Ronaldi <frederic.ronaldi@gmail.com>
2021-10-18 00:37:16 +0700

    Add doriyaki add page

--- src/views/scripts/generator/admin-pages.js
+++ src/views/scripts/generator/admin-pages.js
@@ -50,8 +50,8 @@ const DorayakiDetailsPage = () => {
             <h3>Description</h3>
             <p>{description}</p>
           </div>
-          <button class="doriyaki-button primary">Edit Doriyaki</button>
-          <button class="doriyaki-button outline">Delete Doriyaki</button>
+          <button class="doriyaki-button primary">Edit Dorayaki</button>
+          <button class="doriyaki-button outline">Delete Dorayaki</button>
         </div>
       </div>
 		</div>
@@ -87,6 +87,42 @@ const DorayakiEditPage = () => {
             <textarea name="description" rows="4" cols="50"></textarea>
           </div>
           <input type="submit" class="doriyaki-button primary" value="Save changes" />
+          <button class="doriyaki-button outline">Cancel</button>
+        </form>
+      </div>
+		</div>
+	`;
+
+  target.innerHTML = components;
+}
+
+const DorayakiAddPage = () => {
+  const target = document.getElementById("dorayaki-add-page");
+
+  const components = `
+		${generateNavbarAdmin()}
+		<div class="dorayaki-management-container">
+			${pageTitle("Dorayaki Management")}
+      <div class="doriyaki-details">
+        <img src="" alt="foto doriyaki" id="dorayaki-photo" />
+        <form class="doriyaki-details-main">
+          <div class="doriyaki-details-text">
+            <h3>Name</h3>
+            <input name="name" value="{name}" />
+          </div>
+          <div class="doriyaki-details-text">
+            <h3>Price</h3>
+            <input name="price" type="number" />
+          </div>
+          <div class="doriyaki-details-text">
+            <h3>Stock</h3>
+            <input name="stock" type="number" />
+          </div>
+          <div class="doriyaki-details-text" style="margin-bottom:10px">
+            <h3>Description</h3>
+            <textarea name="description" rows="4" cols="50"></textarea>
+          </div>
+          <input type="submit" class="doriyaki-button primary" value="Add new dorayaki" />
           <button class="doriyaki-button outline">Cancel</button>
         </form>
       </div>
