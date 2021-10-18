89a6bb6488139a589b00aaae33f1603ace312dfa
Marcello Faria <01faria.marcello@gmail.com>
2021-10-18 23:54:43 +0700

    add pagination tinggal button controller

--- src/config/testing.php
+++ src/config/testing.php
@@ -8,13 +8,13 @@ $testing = new Database();
 
 $tes = new DorayakiModel($testing);
 
-$data["name"] = "Tes";
+$data["name"] = "Tess4";
 $data["description"] = "nothing";
 $data["price"] = 30000;
 $data["stock"] = 120;
 $data["thumbnail"] = "nothing";
 
-$tes->createDoriyaki($data);
+$tes->createDorayaki($data);
 
-echo var_dump($tes->getDoriyakis(2));
+echo var_dump($tes->getDorayakis(2));
 //echo var_dump($tes->getAllDoriyakis());
